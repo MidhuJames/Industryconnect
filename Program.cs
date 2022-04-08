6663a60c1d038c3db4db81b8ed38ebb89caa4b73
MidhuJames <midhucherian@gmail.com>
2022-04-08 20:13:22 +1200

    create new material record

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 
 namespace industryconnect
 {
@@ -29,7 +30,7 @@ namespace industryconnect
 
             //Check if user is login successfully
             IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
-            if(helloHari.Text=="Hello hari!")
+            if (helloHari.Text == "Hello hari!")
             {
                 Console.WriteLine("Logged in Successfully: Test Passed");
 
@@ -38,7 +39,65 @@ namespace industryconnect
             {
                 Console.WriteLine("Login Failed: Test Failed");
             }
+            // Create Time amd Material Module
 
+            // Go to Time and Material page
+
+            IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            administrationDropdown.Click();
+
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click();
+
+            // Create new record
+
+            IWebElement recordButton = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+            recordButton.Click();
+
+            // Create Material option
+
+            IWebElement typecodeDropdown = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[1]"));
+            typecodeDropdown.Click();
+
+            IWebElement materialOption = driver.FindElement(By.XPath("//*[@id='TypeCode_listbox']/li[1]"));
+            materialOption.Click();
+
+            //Type Code
+            IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+            codeTextbox.SendKeys("IndustryConnect");
+
+            //Type Description
+            IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+            descriptionTextbox.SendKeys("industry connect");
+
+            //Type Price per unit
+            IWebElement priceTag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+            priceTag.Click();
+            IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+            priceTextbox.SendKeys("12");
+
+            //save button
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Thread.Sleep(1000);
+
+            //click to go to last page
+            IWebElement lastPageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+            lastPageButton.Click();
+            Thread.Sleep(1000);
+
+            // check if record is created in the table and has expected value
+            IWebElement actualCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+
+            if (actualCode.Text == "IndustryConnect")
+            {
+                Console.WriteLine("Material Record created Successfully: Test Passed");
+
+            }
+            else
+            {
+                Console.WriteLine("Material Record created Failed: Test Failed");
+            }
         }
     }
 }
